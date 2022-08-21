a5b1b1cfbdc5df32312a71d9ace28f2d20499d78
semihgurbuz <semihgurbuz.dev@gmail.com>
2022-08-22 00:13:08 +0300

    finished sidebar and header

---
+++ script.js
@@ -0,0 +1,11 @@
+// let sidebarList = document.querySelectorAll("h2");
+
+// function addClass(sidebarList) {
+//   for (let i = 0; i < sidebarList.length; i++) {
+//     const element = sidebarList[i];
+//     let class1 = element.innerHTML.toLowerCase();
+//     element.classList.add(class1);
+//   }
+// }
+
+// addClass(sidebarList);
