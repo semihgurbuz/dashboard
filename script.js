6da55db3ef1822dd0e00841314db8db6070aeb48
semihgurbuz <semihgurbuz.dev@gmail.com>
2022-08-22 19:08:38 +0300

    fixed image paths so that they show up on github pages

--- script.js
+++ script.js
@@ -9,3 +9,14 @@
 // }
 
 // addClass(sidebarList);
+
+// let images = document.querySelectorAll("img");
+
+// function setDirectory(array) {
+//   for (i = 0; i < array.length; i++) {
+//     let element = array[i];
+//     element.src = "." + element.src;
+//   }
+// }
+
+// setDirectory(images);
